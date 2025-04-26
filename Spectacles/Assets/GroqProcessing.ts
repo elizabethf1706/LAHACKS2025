1edf13212f85d985731eedd5bcf6ae4cec36a94c
Lucas Kim <lucaskamadakim@gmail.com>
2025-04-26 10:52:16 -0700

    Basic Groq API

--- Spectacles/Assets/GroqProcessing.ts
+++ Spectacles/Assets/GroqProcessing.ts
@@ -1,4 +1,4 @@
-const GROQ_API_KEY = "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";
+const GROQ_API_KEY = "";
 
 const remoteServiceModule = require("LensStudio:RemoteServiceModule");
 
@@ -14,41 +14,45 @@ export class GroqProcessing extends BaseScriptComponent {
         this.createEvent("UpdateEvent").bind(this.update.bind(this));
     }
 
-    private async update() {
+    private update() {
         const curText = this.transcriptionTextComponent.text;
 
         if (this.prevText !== curText) {
-            const body = {
-                "model": "llama-3.1-8b-instant",
-                "messages": [{
-                    "role": "user",
-                    "content": `This is some dialogue about a company. Reply with some useful information about this company.\n\n${curText}`
-                }]
-            };
+            this.getInfo(curText);
 
-            const headers = {
-                "Content-Type": "application/json",
-                Authorization: `Bearer ${GROQ_API_KEY}`
-            };
+            print("text changed! " + this.prevText + " | " + curText);
 
-            // const request = new Request("https://api.groq.com/openai/v1/chat/completions", {
-            //     method: "POST",
-            //     headers,
-            //     body: JSON.stringify(body),
-            // });
-
-            // const response = await remoteServiceModule.fetch(request);
-                
-            // const responseText = response.choices[0].message.content;
-            // if (responseText.toLowerCase() === "yes") {
-            //     // something here
-            // } else {
-            //     // something else here
-            // }
-
-            print("text changed! ");
+            this.prevText = curText;
         }
 
-        this.prevText = curText;
+        
+    }
+
+    private async getInfo(text: string) {
+        const body = {
+            "model": "llama-3.1-8b-instant",
+            "messages": [{
+                "role": "user",
+                "content": `This is some dialogue about a company. Reply with some useful information about this company.\n\n${text}`
+            }]
+        };
+
+        const headers = {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${GROQ_API_KEY}`
+        };
+
+        const request = new Request("https://api.groq.com/openai/v1/chat/completions", {
+            method: "POST",
+            headers,
+            body: JSON.stringify(body),
+        });
+
+        const response = await remoteServiceModule.fetch(request);
+        const responseJson = await response.json();
+            
+        const responseText = responseJson.choices[0].message.content;
+
+        print(responseText);
     }
 }
